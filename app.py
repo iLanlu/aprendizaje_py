91215dd50fc420cb9a6654eaa7bed93390edf1e3
MichaelCastillo <maicolestebancastillolopez@gmail.com>
2026-08-19 14:07:10 -0500

    Mi primer commit con Flask

--- app.py
+++ app.py
@@ -4,9 +4,14 @@ app = Flask(__name__)
 
 name = "Elba Zurita"
 
-@app.route("/")
+@app.route('/')
 def hello_world():
+    return render_template('hola.html', person=name)
+
+@app.route('/index')
+def Burger():
     return render_template('index.html', person=name)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
